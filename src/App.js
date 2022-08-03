02a39d5a495e87200a679a0784e17080dfdaae42
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-04 00:22:26 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
 import { Container } from '@material-ui/core';
 import Home from './components/Home/Home';
+import PostDetails from './components/PostDetails/PostDetails';
 import Auth from './components/Auth/Auth';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_Auth_Google_ID}`}>
       <BrowserRouter>
-        <Container maxWidth={'lg'}>
+        <Container maxWidth={'xl'}>
           <Navbar />
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/auth' exact component={Auth} />
+            <Route
+              path='/'
+              exact
+              component={() => <Redirect to={'/posts'} />}
+            />
+            <Route path={'/posts'} exact component={Home} />
+            <Route path={'/posts/search'} exact component={Home} />
+            <Route path={'/posts/:id'} exact component={PostDetails} />
+            <Route
+              path='/auth'
+              exact
+              component={() => (!user ? <Auth /> : <Redirect to={'/posts'} />)}
+            />
           </Switch>
         </Container>
       </BrowserRouter>
