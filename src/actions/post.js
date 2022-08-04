537877f428cca3b533eb15f0688f2eb65c4b5b2a
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-04 13:09:36 +0530

    api search

--- src/actions/post.js
+++ src/actions/post.js
@@ -21,7 +21,9 @@ export const getPosts = () => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPostsBySearch(searchQuery);
+    const {
+      data: { data },
+    } = await api.fetchPostsBySearch(searchQuery);
 
     // dispatch({ type: FETCH_ALL, payload: data });
     console.log(data);
